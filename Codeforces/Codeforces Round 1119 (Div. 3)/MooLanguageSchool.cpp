cea9347aea985f05e33d912bbff249db1ee03ce1
MD Raihan Hosen <raihanhosen1381@gmail.com>
2026-09-05 20:51:31 +0600

    Add MooLanguageSchool.cpp solution for Codeforces 1119

---
+++ Codeforces/Codeforces Round 1119 (Div. 3)/MooLanguageSchool.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+ 
+    int ans = 0;
+    for (int i = 0; i < n; i += k)
+    {
+        bool has_zero = false;
+        for (int j = i; j < i + k; j++)
+        {
+            if (s[j] == '0')
+            {
+                has_zero = true;
+                break;
+            }
+        }
+        if (!has_zero)
+        {
+            ans++;
+        }
+    }
+ 
+    cout << ans << endl;
+}
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
